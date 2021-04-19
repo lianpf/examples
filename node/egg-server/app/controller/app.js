aa0d72fbcc1e8ff718bdc4d3e011cd02b9122556
lianpf <wanderlian@foxmail.com>
2021-04-19 16:39:31 +0800

    feat: app 模块 list / detail 统一调用 success

--- node/egg-server/app/controller/app.js
+++ node/egg-server/app/controller/app.js
@@ -6,6 +6,7 @@ const path = require('path');
 const Stream = require('stream');
 const sendToWormhole = require('stream-wormhole');
 // const awaitWriteStream = require('await-stream-ready').write;
+const { getObj, getUuid } = require('../utils/utils.js');
 
 /**
  * @desc: Application 相关api
@@ -15,24 +16,13 @@ class AppController extends Controller {
   async list() {
     const { ctx } = this;
     const appList = await ctx.service.app.list()
-    ctx.body = {
-      status: 200,
-      data: appList,
-      message: appList.length > 0 ? 'success' : '当前无应用'
-    };
-    ctx.status = 200;
+    ctx.helper.success({ctx, res: appList, message: appList.length > 0 ? 'success' : '当前无应用，请先创建应用！'})
   }
   async detail() {
     const { ctx } = this;
     const { appId: app_id = '' } = ctx.query;
     const appDetail = await ctx.service.app.detail(app_id)
-    // console.log('--appDetail--', Array.isArray(appDetail))
-    ctx.body = {
-      status: 200,
-      data: appDetail.length > 0 ? appDetail[0] : {},
-      message: appDetail.length > 0 ? 'success' : '不存在该页面'
-    };
-    ctx.status = 200;
+    ctx.helper.success({ctx, res: getObj(appDetail), message: appDetail.length > 0 ? 'success' : '不存在该页面'})
   }
 }
 

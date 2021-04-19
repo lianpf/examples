df8db9b21ef59851cb110cd9da1eeb274142c950
lianpf <wanderlian@foxmail.com>
2021-04-19 16:38:27 +0800

    refactor: helper 函数增加 ctx.response 统一状态信息处理

--- node/egg-server/app/extend/helper.js
+++ node/egg-server/app/extend/helper.js
@@ -5,3 +5,47 @@
  * */
 const moment = require('moment');
 exports.relativeTime = time => moment(new Date(time * 1000)).fromNow();
+
+// 处理成功响应
+exports.success = ({ ctx, res = null, message = '请求成功' })=> {
+  ctx.body = {
+    status: 200,
+    code: 0,
+    data: res,
+    message
+  }
+  ctx.status = 200
+}
+
+// 处理失败（客户端）
+exports.clientError = ({ ctx, res = null, message = '参数缺失' })=> {
+  ctx.body = {
+    status: 400,
+    code: 0,
+    data: res,
+    message
+  }
+  ctx.status = 400
+}
+
+// 处理失败（客户端）
+exports.serverError = ({ ctx, res = null, message = '服务端异常' })=> {
+  ctx.body = {
+    status: 500,
+    code: 0,
+    data: res,
+    message
+  }
+  ctx.status = 500
+}
+
+// 自定义 response
+exports.customRes = ({ ctx, res = null, message = '服务端异常', status = 200, code = 0 })=> {
+  ctx.body = {
+    status,
+    code,
+    data: res,
+    message
+  }
+  ctx.status = status
+}

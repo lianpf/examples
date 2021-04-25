38eb668bc9623da0fd92ffc5eb50dba9505b9533
lianpf <wanderlian@foxmail.com>
2021-04-25 16:28:28 +0800

    feat: 定义 api 路由

--- node/egg-server/app/router.js
+++ node/egg-server/app/router.js
@@ -8,9 +8,18 @@ module.exports = app => {
   router.get('/', controller.home.index);
   router.get('/api', controller.api.list);
   router.post('/api/upload', controller.api.upload);
+  // 应用模块
   router.get('/api/app/list', controller.app.list);
   router.get('/api/app/detail', controller.app.detail);
+  // page 模块
   router.get('/api/page/list', controller.page.list);
   router.get('/api/page/detail', controller.page.detail);
   router.post('/api/page/upload-code', controller.page.uploadCode);
+  // 资产模块
+  router.get('/api/assets/list', controller.assets.list);
+  router.post('/api/assets/create', controller.assets.create);
+  router.get('/api/assets/brief-info', controller.assets.briefInfo);
+  // util模块
+  router.post('/api/upload/schema', controller.common.uploadSchema);
+  router.get('/api/custom-code/detail', controller.common.getCustomCode);
 };
